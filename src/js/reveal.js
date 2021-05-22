e0c562df4627d98524af8e55b71af75815f25e64
nasrah01 <nasrah1@hotmail.co.uk>
2021-05-22 20:54:50 +0100

    gsap change for about section

--- src/js/reveal.js
+++ src/js/reveal.js
@@ -1,26 +1,48 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger.js";
-gsap.registerPlugin(ScrollTrigger);
-
 
 export const animate = () => {
-
-  const boxes = gsap.utils.toArray('.boxes__box');
-
-  gsap.from(boxes, {
-    yPercent: 100,
-    opacity: 0,
-    ease: "slow(0.7, 0.7, false)",
-    duration: 1.5,
-    stagger: 1,
-    autoAlpha: 1,
-    scrollTrigger: {
-      trigger: '.about__content',
-      start: 'top center',
-      scrub: true
+  function animateFrom(elem, direction) {
+    direction = direction || 1;
+    var x = 0,
+        y = direction * 100;
+    if(elem.classList.contains("boxes__box--02")) {
+      x = -100;
+      y = 0;
+    } else if (elem.classList.contains("boxes__box--04")) {
+      x = 100;
+      y = 0;
     }
-  })
-
+    elem.style.transform = "translate(" + x + "px, " + y + "px)";
+    elem.style.opacity = "0";
+    gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+      duration: 1.25, 
+      x: 0,
+      y: 0, 
+      autoAlpha: 1, 
+      ease: "expo", 
+      overwrite: "auto"
+    });
+  }
+  
+  function hide(elem) {
+    gsap.set(elem, {autoAlpha: 0});
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    gsap.registerPlugin(ScrollTrigger);
+    
+    gsap.utils.toArray(".boxes__box").forEach(function(elem) {
+      hide(elem); // assure that the element is hidden when scrolled into view
+      
+      ScrollTrigger.create({
+        trigger: elem,
+        onEnter: function() { animateFrom(elem) }, 
+        onEnterBack: function() { animateFrom(elem, -1) },
+        onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
+      });
+    });
+  });
 };
 
 
